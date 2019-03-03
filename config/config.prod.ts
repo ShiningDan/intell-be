4c7d92e685d8f54f368af1c17ea2ad2b537844fa
ShiningDan <zhuce.zyc@gmail.com>
2019-03-03 15:32:19 +0800

    feat(database): 线上线下数据库分离

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,5 +2,22 @@ import { EggAppConfig, PowerPartial } from 'egg'
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {}
+
+  config.typeorm = {
+    type: 'mysql',
+    entityPrefix: '',
+    charset: 'utf8mb4',
+    host: 'cdb-fvy0fdr4.bj.tencentcdb.com',
+    port: '10047',
+    username: 'root',
+    password: 'aVfXoj2UAVTiVj',
+    database: 'test',
+    connectTimeout: 60000,
+    synchronize: true,
+    logging: false,
+    entities: ['app/model/**/*.ts'],
+    migrations: ['app/migration/**/*.ts'],
+    subscribers: ['app/subscriber/**/*.ts']
+  }
   return config
 }
